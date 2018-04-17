7812a907c5e280eff176a19f27dcdaca2a9a653a
Brian <akalarrybird@gmail.com>
2018-04-17 14:34:04 -0700

    Adding getters and setters for entity class

--- src/game/MainClass.java
+++ src/game/MainClass.java
@@ -9,9 +9,10 @@ public class MainClass {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Snake");
         frame.setContentPane(new GamePanel());
-        frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
+        frame.pack();
+        
         frame.setPreferredSize(new Dimension(GamePanel.WIDTH, GamePanel.HEIGHT));
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
